28294188a17f676a0c3b077d23f03ee5f39e900e
LePichu <ishat.prsnl65844@gmail.com>
2021-07-12 12:11:58 +0530

    quick tweaks

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,29 @@
 import 'phaser';
+import { Scale } from 'phaser';
 
-export default class Demo extends Phaser.Scene
+export default class TitleScreen extends Phaser.Scene
 {
-    constructor ()
-    {
-        super('demo');
+    constructor() {
+        super('TitleScreen');
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    preload() {
+        
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    create() {
+        
     }
 }
 
+const scale = 8
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+    backgroundColor: '#010101',
+    autoCenter: true,
+    width: 240 * scale,
+    height: 160 * scale,
+    scene: TitleScreen
 };
 
 const game = new Phaser.Game(config);
